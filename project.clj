00254e5537501a5edc44c011c762298c10662107
Daniel Cunha (soro) <danielsoro@gmail.com>
2015-03-09 14:12:26 -0300

    Updated license

--- project.clj
+++ project.clj
@@ -1,8 +1,23 @@
+;;Copyright 2015 Daniel Cunha (soro) and/or its affiliates and other contributors
+;;as indicated by the @authors tag. All rights reserved.
+;;
+;;Licensed under the Apache License, Version 2.0 (the "License");
+;;you may not use this file except in compliance with the License.
+;;You may obtain a copy of the License at
+;;
+;;    http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;Unless required by applicable law or agreed to in writing, software
+;;distributed under the License is distributed on an "AS IS" BASIS,
+;;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;See the License for the specific language governing permissions and
+;;limitations under the License.
 (defproject tomee-cli "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "CLI to configure Apache TomEE"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License"
+            :version "2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot tomee-cli.core
   :target-path "target/%s"
--- src/tomee_cli/core.clj
+++ src/tomee_cli/core.clj
@@ -1,3 +1,17 @@
+;;Copyright 2015 Daniel Cunha (soro) and/or its affiliates and other contributors
+;;as indicated by the @authors tag. All rights reserved.
+;;
+;;Licensed under the Apache License, Version 2.0 (the "License");
+;;you may not use this file except in compliance with the License.
+;;You may obtain a copy of the License at
+;;
+;;    http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;Unless required by applicable law or agreed to in writing, software
+;;distributed under the License is distributed on an "AS IS" BASIS,
+;;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;See the License for the specific language governing permissions and
+;;limitations under the License.
 (ns tomee-cli.core
   (:require [clojure.java.shell :refer (sh)])
   (:gen-class))
@@ -8,7 +22,7 @@
 (defn shutdown []
   (sh "../bin/shutdown.sh"))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;(defn -main
+;;  "I don't do a whole lot ... yet."
+;;  [& args]
+;;  (println "Hello, World!"))
