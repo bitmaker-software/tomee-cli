22e9f00845671690185f2a67edde4795b503b43c
Daniel Cunha (soro) <danielsoro@gmail.com>
2015-03-23 18:59:59 -0300

    Initial implementation for JMS Resource

---
+++ src/tomee_cli/configuration/jms_resource.clj
@@ -0,0 +1,19 @@
+;;Copyright 2015 Bitmaker Software LDA and/or its affiliates and other contributors
+;;as indicated by the @authors tag. All rights reserved.
+;;
+;;Licensed under the Apache License, Version 2.0 (the "License");
+;;you may not use this file except in compliance with the License.
+;;You may obtain a copy of the License at
+;;
+;;    http://www.apache.org/licenses/LICENSE-2.0
+;;
+;;Unless required by applicable law or agreed to in writing, software
+;;distributed under the License is distributed on an "AS IS" BASIS,
+;;WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;;See the License for the specific language governing permissions and
+;;limitations under the License.
+
+(ns ^{:author "Daniel Cunha (soro) <daniel.cunha@bitmaker-software.com>"}
+ tomee-cli.configuration.jms-resource
+  (:require [tomee-cli.resources   :refer (add-resource xml-with-out-str define-resource) :as resource]
+            [tomee-cli.environment :refer (tomee-home) :as environment]))
