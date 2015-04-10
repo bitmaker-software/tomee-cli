e68f0a5a4f4a8d6c67c11c0c0aac1feb36b664d3
Daniel Cunha (soro) <danielsoro@gmail.com>
2015-04-09 23:09:57 -0300

    Fixed build checkstyle

--- src/tomee_cli/installation.clj
+++ src/tomee_cli/installation.clj
@@ -57,10 +57,10 @@
                          (= (str "." (utils/filename-extension (.getName %))) env/extension))
                    files)))))
 
-(defn install-tomee [ & {:keys [dist version location]
-                         :or {dist "webprofile" version "1.7.1" location "."}}]
+(defn install-tomee [& {:keys [dist version location]
+                        :or {dist "webprofile" version "1.7.1" location "."}}]
   (grant-permission
-    (unzip-file
-      (utils/download-file
-        (str "http://apache.belnet.be/tomee/tomee-" version "/apache-tomee-" version "-" dist ".zip")
-        (str location "/apache-tomee-" version "-" dist ".zip")))))
+   (unzip-file
+    (utils/download-file
+     (str "http://apache.belnet.be/tomee/tomee-" version "/apache-tomee-" version "-" dist ".zip")
+     (str location "/apache-tomee-" version "-" dist ".zip")))))
